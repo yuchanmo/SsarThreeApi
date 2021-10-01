a7710bf0d3f1679e98c521a69278c88150169275
yuchanmo <mojjijji@gmail.com>
2021-10-01 23:23:54 +0900

    save

--- resources/artists.py
+++ resources/artists.py
@@ -15,9 +15,10 @@ class Artists(Resource):
         try:
             parser = RequestParser()
             parser.add_argument('artistname', required=True,help="Name cannot be blank!")
+            parser.add_argument('userid', required=True,help="Name cannot be blank!")
             args = parser.parse_args()
-            artist_name = args['artistname']
-            df = pd.read_sql(f"exec searchArtistList '{artist_name}'",sqlserver)
+            artist_name,user_id = args['artistname'],args['userid']
+            df = pd.read_sql(f"exec searchArtistList '{artist_name}',{user_id}",sqlserver)
             df['image_url'] = df[['auction_url_num','image_name','lot_no','auction_site','auction_cate']].apply(lambda x :  f"{image_base_url}/{x['auction_site']}/{x['auction_cate']}/{x['auction_url_num']}/LOT{x['lot_no']}_{x['image_name']}",axis=1)
             return df.to_dict(orient='records')
         except Exception as e:
--- t.py
+++ t.py
@@ -6,8 +6,20 @@ import pandas as pd
 from constant import *
 
 
-que = session.query(following_artists).filter_by(user_id=1)
-que.count()
+row = session.query(following_artists).filter_by(user_id=10,artist_id =3).first()
+
+if row == None and turn_on ==True:
+    newrow = following_artists(user_id = 1,artist_id =15,turn_on = True)
+    session.add(newrow)
+    session.commit()
+
+elif row!=None:
+    row.turn_on = False
+    session.commit()
+
+
+else:
+    session.add(following_artists(user_id=1,))
 for row in que:
     print(row.user_id)
     print(row.artist_id)
